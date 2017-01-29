278cfa4dd7588a823453a16e94881e8acfb00196
Wes Davis <wesley.r.davis@gmail.com>
2017-01-28 22:08:06 -0500

    Reduce ZPL logging

--- ZenPacks/daviswr/OSX/Server/Caching/__init__.py
+++ ZenPacks/daviswr/OSX/Server/Caching/__init__.py
@@ -1,3 +1,4 @@
+# https://zenpack-sdk.zenoss.com/en/2.0.0/changes.html
 from ZenPacks.zenoss.ZenPackLib import zenpacklib
-
-CFG = zenpacklib.load_yaml(verbose=True, level=10)
+CFG = zenpacklib.load_yaml()
+schema = CFG.zenpack_module.schema
