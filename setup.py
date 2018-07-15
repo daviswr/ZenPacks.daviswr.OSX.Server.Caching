4a3dce871090971b3d4336ff2ad947ee24d6ae64
Wes Davis <wesley.r.davis@gmail.com>
2018-07-15 17:21:29 -0400

    REL: Version 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.daviswr.OSX.Server.Caching"
-VERSION = "0.9.3"
+VERSION = "1.0.0"
 AUTHOR = "Wes Davis"
 LICENSE = "MIT"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.daviswr', 'ZenPacks.daviswr.OSX', 'ZenPacks.daviswr.OSX.Server']
